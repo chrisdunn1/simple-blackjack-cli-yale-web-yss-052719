1ed35245bde6b5c2b97acbb3a531ee549ca0dfe2
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-11 15:25:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,16 +41,6 @@ end
 
 
 
-
-describe "#end_game" do
-  it "prints apology, card total, and thank you message" do
-    expect($stdout).to receive(:puts).with("Sorry, you hit 27. Thanks for playing!")
-    end_game(27)
-  end
-end
-
-
-
 def initial_round
   # code #initial_round here
 end
